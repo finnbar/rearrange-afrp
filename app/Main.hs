338c56918777df37c22790ab3134f03f588733e9
Finnbar Keating <finnjkeating@gmail.com>
2024-01-16 17:17:25 +0000

    Tested with a few proc notation examples

--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,17 @@
 
 module Main (main) where
 
+-- TODO: GeneralisedArrow import is needed because of proc's reliance on qualified modules. Try to avoid?
 import RAFRP
+import GenProc.GeneralisedArrow
+import GenProc.ProcTH (gap)
 
 arr1 :: AFRP _ (V Int) (P (V Int) (V Int))
 arr1 = Arr (\(One x) -> let y = x + 1 in Pair (One y) (One y))
 
 -- NOTE: The Arr id are added to avoid the Pre bug. The bug still needs fixing.
 prog1 :: AFRP _ (V Int) (P (V Int) (V Int))
-prog1 = arr1 :>>>: (Arr (\(One x) -> One (x + 1)) :***: Pre (One 1)) :>>>: Arr id
+prog1 = arr1 :>>>: (Arr (\(One x) -> One (x + 1)) :***: Pre (One 1)) :>>>: Arr Prelude.id
 
 prog1Run :: IO (Val (V Int) -> IO (Val (P (V Int) (V Int))))
 prog1Run = makeAFRP prog1
@@ -18,7 +21,7 @@ arr2 :: AFRP _ (P (V Int) (V Int)) (P (V Int) (V Int))
 arr2 = Arr (\(Pair (One x) (One y)) -> let z = x + y in Pair (One z) (One z))
 
 prog2 :: AFRP _ (V Int) (V Int)
-prog2 = Loop (arr2 :>>>: (Id :***: Pre (One 0))) :>>>: Arr id
+prog2 = Loop (arr2 :>>>: (Id :***: Pre (One 0))) :>>>: Arr Prelude.id
 
 prog2Run :: IO (Val (V Int) -> IO (Val (V Int)))
 prog2Run = makeAFRP prog2
@@ -31,6 +34,26 @@ prog3 = arr1 :>>>: Dup :>>>: (arr2 :***: Pre (Pair (One 1) (One 2))) :>>>: (Drop
 
 prog3Run :: IO (Val (V Int) -> IO (Val (V Int)))
 prog3Run = makeAFRP prog3
+
+prog4 :: AFRP _ (P (V Int) (V Int)) (V Int)
+prog4 = [gap|proc {x,y} -> do
+        {z, w} <- arr1 -< x
+        arr3 -< {z, y}
+    |]
+
+prog4Run :: IO (Val (P (V Int) (V Int)) -> IO (Val (V Int)))
+prog4Run = makeAFRP prog4
+
+prog5 :: AFRP _ (V Int) (V Int)
+prog5 = [gap|proc x -> do
+        rec
+            z <- arr3 -< {x, prev}
+            prev <- pre (One 0) -< z
+        returnA -< z
+    |]
+
+prog5Run :: IO (Val (V Int) -> IO (Val (V Int)))
+prog5Run = makeAFRP prog5
 
 main :: IO ()
 main = do
@@ -52,4 +75,16 @@ main = do
     print out13
     print out23
 
--- [5,1], [6,4], 3, 7, 10, 14
+    runnable4 <- prog4Run
+    out14 <- runnable4 (Pair (One 3) (One 4))
+    out24 <- runnable4 (Pair (One 6) (One 5))
+    print out14
+    print out24
+
+    runnable5 <- prog5Run
+    out15 <- runnable5 (One 3)
+    out25 <- runnable5 (One 4)
+    print out15
+    print out25
+
+-- [5,1], [6,4], 3, 7, 10, 14, 8, 12, 3, 7
