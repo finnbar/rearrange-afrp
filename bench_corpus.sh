de46fb295f4a7771c97c2172cbb28d5b9c11b4a6
Finnbar Keating <finnjkeating@gmail.com>
2024-02-07 16:22:33 +0000

    Split up many_bench for easier repeated testing

---
+++ bench_corpus.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+touch test-results.csv
+for t in corpus/*
+do
+    cp $t generated/Test0.hs
+    # Benchmark!
+    stack bench
+    # Save the CSV for later.
+    cat "tests.csv" | tail -n 2 >> test-results.csv
+done
---
+++ many_gen.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p corpus
+for ITERATION in 1 2 3 4 5 6 7 8 9 10; do
+    for SIZE in 10 20 30; do
+        for LOOPFRAC in 0 0.2 0.5 1; do
+            # Build a program using those parameters.
+            stack run $SIZE $LOOPFRAC
+            # Save that program.
+            cp "generated/Test0.hs" "corpus/Test-$ITERATION-$SIZE-$LOOPFRAC.hs"
+       done
+    done
+done
